281c579ca97b3f643c43a84e251f7d246966d7f4
linfuqing <linfuqing@126.com>
2026-05-20 15:48:58 +0800

    Update InstanceManagerEditor.cs

--- ZG.Entities.Rendering.Managed.Editor/InstanceManagerEditor.cs
+++ ZG.Entities.Rendering.Managed.Editor/InstanceManagerEditor.cs
@@ -14,39 +14,48 @@ namespace ZG
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginProperty(position, label, property);
-            
+
             float singleLineHeight = EditorGUIUtility.singleLineHeight;
             position.height = singleLineHeight;
-            EditorGUI.PropertyField(position, property.FindPropertyRelative("name"));
-            
-            position.y += singleLineHeight;
-            EditorGUI.PropertyField(position, property.FindPropertyRelative("destroyMessageName"));
-            
-            position.y += singleLineHeight;
-            EditorGUI.PropertyField(position, property.FindPropertyRelative("destroyMessageValue"));
-            
-            position.y += singleLineHeight;
+            bool isExpanded = property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, label);
+            if (isExpanded)
+            {
+                ++EditorGUI.indentLevel;
+                
+                position.y += singleLineHeight;
+                EditorGUI.PropertyField(position, property.FindPropertyRelative("name"));
 
-            var gameObjectName = property.FindPropertyRelative("gameObjectName");
-            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(gameObjectName.stringValue);
-            
-            prefab = EditorGUI.ObjectField(position, "Prefab", prefab, typeof(GameObject), false) as GameObject;
-            
-            gameObjectName.stringValue = prefab == null ? string.Empty : AssetDatabase.GetAssetPath(prefab);
-            
+                position.y += singleLineHeight;
+                EditorGUI.PropertyField(position, property.FindPropertyRelative("destroyMessageName"));
+
+                position.y += singleLineHeight;
+                EditorGUI.PropertyField(position, property.FindPropertyRelative("destroyMessageValue"));
+
+                position.y += singleLineHeight;
+
+                var gameObjectName = property.FindPropertyRelative("gameObjectName");
+                var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(gameObjectName.stringValue);
+
+                prefab = EditorGUI.ObjectField(position, "Prefab", prefab, typeof(GameObject), false) as GameObject;
+
+                gameObjectName.stringValue = prefab == null ? string.Empty : AssetDatabase.GetAssetPath(prefab);
+
 #if !INSTANCE_ASSET_STREAMING
             property.FindPropertyRelative("gameObject").objectReferenceValue = prefab;
 #endif
-            
-            position.y += singleLineHeight;
-            EditorGUI.PropertyField(position, property.FindPropertyRelative("destroyTime"));
-            
+
+                position.y += singleLineHeight;
+                EditorGUI.PropertyField(position, property.FindPropertyRelative("destroyTime"));
+
+                --EditorGUI.indentLevel;
+            }
+
             EditorGUI.EndProperty();
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            return property.isExpanded ? EditorGUIUtility.singleLineHeight * 5.0f : EditorGUIUtility.singleLineHeight;
+            return property.isExpanded ? EditorGUIUtility.singleLineHeight * 6.0f : EditorGUIUtility.singleLineHeight;
         }
     }
 }
